eb44d7aaa031948f2b249bf2d0fc1685afb895fd
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-20 19:48:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,6 @@ var chocolateBars =['snickers','hundred grand','kitkat','skittles']
 function addElementToBeginningOfArray(){
   
 }
-function destructivelyAddElementToBeginningOfArray
+function destructivelyAddElementToBeginningOfArray(){
+  
+}
