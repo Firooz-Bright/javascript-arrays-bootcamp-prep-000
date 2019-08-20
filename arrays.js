214bb828b9951e369c766bf9838abe51f5bb0876
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-20 20:01:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,8 @@
 var chocolateBars =['snickers','hundred grand','kitkat','skittles']
 
 function addElementToBeginningOfArray(arry, element){
-  var arr=[]
-  arr.push(arry)
-  return arr.unshift(element)
+  
+  return arry[]
 }
 function destructivelyAddElementToBeginningOfArray(arry, element){
   var arr=[]
